3ddd79a3d37f67017569fcee35b7736a4a821c42
lintanganugerah <lintanganugerah4@gmail.com>
2023-01-03 23:33:40 +0700

    template

---
+++ Zellenials laravel/resources/views/layout/layout.blade.php
@@ -0,0 +1,35 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Zellenials | Made by Passion </title>
+    <link rel="Zellenials icon" href="/Zellenials/Assets/favicon.ico">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+<body style="background-color: #1f1f1f;">
+    @yield('content')
+    <footer>
+        <div class="d-flex" style="background-color: #C5FF01; margin-top:200px; padding-left: 70px; padding-right: 70px;">
+            <div class="p-3">
+              <ul class="list-group-border-width: 0px;" style="padding-top: 20px;">
+              <li class="list-group-item"><b>Zellenials Company</b></li>
+              <li class="list-group-item">Brand lokal yang menjual produk <br>kaos bertemakan streetwear dengan <br>kualitas kain dan sablon yang tinggi.</li>
+            </div>
+            <div class="p-3 flex-grow-1">
+              <ul class="list-group-border-width: 0px;" style="padding-top: 20px;">
+              <li class="list-group-item"><b>Produk</b></li>
+              <li class="list-group-item">Kaos Streetwear</li>
+              <li class="list-group-item">Hoodie</li>
+            </div>
+            <div class="p-3">
+            <ul class="list-group-border-width: 0px;" style="padding-top: 20px;">
+              <li class="list-group-item"><b>Kontak</b></li>
+              <li class="list-group-item"><img src="{{asset('storage/Assets/WhatsApp.png')}}" alt="whatsapp.png"> 085161252804</li>
+              <li class="list-group-item"><img src="{{asset('storage/Assets/envelope.png')}}" alt="envelope.png"> zellenialscompany@gmail.com</li>
+              <li class="list-group-item"><img src="{{asset('storage/Assets/Instagram.png')}}" alt="instagram.png"> zellenials.co</li>
+            </div>
+          </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</body>
+</html>
