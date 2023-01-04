06ba194abebc5d2792c2259a1d49916ef58781a0
afdalcitra <afdalcitra.edu@gmail.com>
2023-01-04 10:24:26 +0700

    homepage_edit - afdal

--- Zellenials/Pages/User/homepage.php
+++ Zellenials/Pages/User/homepage.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Zellenials | Made by Passion </title>
-    <link rel="Zellenials icon" href="/Zellenials/Assets/favicon.ico">
+    <link rel="Zellenials icon" href="../../Assets/favicon.ico">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
   </head>
   <body style="background-color: #1f1f1f;">
@@ -13,7 +13,7 @@
 
     <header>
 
-      <?php require ('../../Templates/navbar-old.php');?>
+      <?php require('../../Templates/navbar-old.php')?>
 
     </header>
 
